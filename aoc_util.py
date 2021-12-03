8ea49f0556ef5a2bf285cbbb9808e4d9f3d3522b
Henrik Zimmermann <henrik@zi-data.com>
2021-12-03 16:36:13 -0500

    Created a module to help with doing the tasks faster

---
+++ aoc_util.py
@@ -0,0 +1,51 @@
+import os
+from typing import Callable
+import requests
+import pyperclip
+
+
+def download_data_set(day:int) -> list[str]:
+    input_data = ""
+
+    with    open("session_cookie.txt") as session_file, \
+            open(f"data_day{day}.txt", "w") as data_file:
+        url = f"https://adventofcode.com/2021/day/{day}/input"
+        session_cookie = session_file.readline()
+        cookies = { "session": session_cookie }
+
+        response = requests.get(url, cookies=cookies)
+        input_data = response.content.decode("ascii")[:-1]
+        data_file.write(input_data)
+
+    return input_data.split("\n")
+
+
+def get_data_set(day: int) -> list[str]:
+    ret = []
+    data_file_path = f"./data_day{day}.txt"
+    if os.path.isfile(data_file_path):
+        with open(data_file_path) as data_file:
+            ret = data_file.readlines()
+    else:
+        ret = download_data_set(day)
+
+    return ret
+
+
+def run_with_data_set(fun: Callable, day: int):
+    data_set = get_data_set(day)
+
+    run(fun, data_set)
+
+
+def run_with_data_str(fun: Callable, data_str: str):
+    data_set = data_str.split("\n")
+
+    run(fun, data_set)
+
+
+def run(fun: Callable, data_set: list[str]):
+    result = fun(data_set)
+
+    print(result)
+    pyperclip.copy(result)
---
+++ template.py
@@ -0,0 +1,17 @@
+###############################################################################
+# Day x, Task y                                                               #
+###############################################################################
+
+import aoc_util
+
+day = 0
+data_str = """
+"""
+
+
+def task(data_set: list[str]) -> int:
+    pass
+
+
+aoc_util.run_with_data_str(task, data_str)
+aoc_util.run_with_data_set(task, day)
