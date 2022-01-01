9c94ff04067838720105005ecfb33c9a94e0235a
Henrik Zimmermann <henrik@zi-data.com>
2022-01-01 18:31:05 +0100

    Day 21 done.

--- day21_task2.py
+++ day21_task2.py
@@ -22,31 +22,26 @@ dice_roll_to_n = {
 
 
 def play(n, pos_1, pos_2, score_1, score_2):
-
+    if score_2 >= 21:
+        return 0, n
+    
     player_1_wins = 0
     player_2_wins = 0
-    for p1 in range(3, 10):
-        for p2 in range(3, 10):
-            new_pos_1 = (pos_1 + p1) % 10
-            new_score_1 = score_1 + new_pos_1 + 1
-            new_pos_2 = (pos_2 + p2) % 10
-            new_score_2 = score_2 + new_pos_2 + 1
 
-            if new_score_1 >= 21:
-                player_1_wins += n
-            elif new_score_2 >= 21:
-                player_2_wins += n
-            else:
-                player_1_wins_tmp, player_2_wins_tmp = play(
-                    n * dice_roll_to_n[p1] * dice_roll_to_n[p2],
-                    new_pos_1,
-                    new_pos_2,
-                    new_score_1,
-                    new_score_2
-                    )
+    for p in range(3, 10):
+        new_pos_1 = (pos_1 + p) % 10
+        new_score_1 = score_1 + new_pos_1 + 1
+        
+        player_2_wins_tmp, player_1_wins_tmp = play(
+        n * dice_roll_to_n[p],
+        pos_2,
+        new_pos_1,
+        score_2,
+        new_score_1
+        )
 
-                player_1_wins += player_1_wins_tmp
-                player_2_wins += player_2_wins_tmp
+        player_1_wins += player_1_wins_tmp
+        player_2_wins += player_2_wins_tmp
 
     return player_1_wins, player_2_wins
 
