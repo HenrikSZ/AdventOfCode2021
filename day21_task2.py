fd624b843ba3bdd1575db402eea5ad76782d580d
Henrik Zimmermann <henrik@zi-data.com>
2022-01-01 18:00:42 +0100

    Day 21 very slow.

--- day21_task2.py
+++ day21_task2.py
@@ -21,63 +21,41 @@ dice_roll_to_n = {
 }
 
 
-def play(steps_to_universes, n, pos, score, step_count):
-    step_count += 1
-    for p in range(3, 10):
-        new_pos = (pos + p) % 10
-        new_score = score + new_pos + 1
+def play(n, pos_1, pos_2, score_1, score_2):
 
-        if new_score >= 21:
-            steps_to_universes[step_count] = steps_to_universes.get(step_count, 0) + n
-        else:
-            play(steps_to_universes,
-                n * dice_roll_to_n[p],
-                new_pos,
-                new_score,
-                step_count
-                )
+    player_1_wins = 0
+    player_2_wins = 0
+    for p1 in range(3, 10):
+        for p2 in range(3, 10):
+            new_pos_1 = (pos_1 + p1) % 10
+            new_score_1 = score_1 + new_pos_1 + 1
+            new_pos_2 = (pos_2 + p2) % 10
+            new_score_2 = score_2 + new_pos_2 + 1
+
+            if new_score_1 >= 21:
+                player_1_wins += n
+            elif new_score_2 >= 21:
+                player_2_wins += n
+            else:
+                player_1_wins_tmp, player_2_wins_tmp = play(
+                    n * dice_roll_to_n[p1] * dice_roll_to_n[p2],
+                    new_pos_1,
+                    new_pos_2,
+                    new_score_1,
+                    new_score_2
+                    )
+
+                player_1_wins += player_1_wins_tmp
+                player_2_wins += player_2_wins_tmp
+
+    return player_1_wins, player_2_wins
 
 
 def task(data_set: list[str]) -> int:
     pos_1 = int(data_set[0][-1]) - 1
     pos_2 = int(data_set[1][-1]) - 1
 
-    steps_to_universes_1 = {}
-    steps_to_universes_2 = {}
-
-    play(steps_to_universes_1, 1, pos_1, 0, 0)
-    play(steps_to_universes_2, 1, pos_2, 0, 0)
-
-    sorted_keys_1 = sorted(steps_to_universes_1.keys())
-    sorted_keys_2 = sorted(steps_to_universes_2.keys())
-
-    player_1_wins = 0
-    player_2_wins = 0
-
-    sum_1 = sum(steps_to_universes_1.values())
-    sum_2 = sum(steps_to_universes_2.values())
-
-    for i in sorted_keys_1:
-        mult = steps_to_universes_1[i]
-        acc = 0
-
-        for j in sorted_keys_2:
-            if i <= j:
-                acc += steps_to_universes_2[j]
-
-        player_1_wins += acc * mult
-
-
-    for i in sorted_keys_2:
-        mult = steps_to_universes_2[i]
-        acc = 0
-
-        for j in sorted_keys_1:
-            if i < j:
-                acc += steps_to_universes_1[j]
-
-        player_2_wins += acc * mult
-
+    player_1_wins, player_2_wins = play(1, pos_1, pos_2, 0, 0)
 
     return max(player_1_wins, player_2_wins)
 
