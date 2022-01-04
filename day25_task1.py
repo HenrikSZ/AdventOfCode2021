8a0d66e9379dea67d722c65526bf088a65f7d037
Henrik Zimmermann <henrik@zi-data.com>
2022-01-04 14:02:36 +0100

    Day 25 done.

--- day25_task1.py
+++ day25_task1.py
@@ -6,7 +6,7 @@ import aoc_util
 
 
 day = 25
-data_str_easy = """...>...
+data_steasy = """...>...
 .......
 ......>
 v.....>
@@ -25,44 +25,50 @@ v.v..>>v.v
 
 
 def task(data_set: list[str]) -> int:
-    r_grid = []
-    d_grid = []
+    grid = []
 
     for l in data_set:
         row = [c for c in l]
-        r_grid.append(row)
-        d_grid.append(row.copy())
+        grid.append(row)
 
     steps = 0
 
     moved = True
     while moved:
         moved = False
-        for row in range(len(r_grid)):
-            for col in range(len(r_grid[0])):
-                if r_grid[row][col] == "." and r_grid[row][col - 1] == ">":
-                    d_grid[row][col] = ">"
-                    d_grid[row][col - 1] = "."
+        for row in range(len(grid)):
+            last_was_occupied = False
+            col = 0
+            while col < len(grid[0]):
+                if grid[row][col] == "." and grid[row][col - 1] == ">" \
+                    and (col != len(grid[0]) - 1 or not last_was_occupied):
+                    if col == 0:
+                        last_was_occupied = True
+                    grid[row][col] = ">"
+                    grid[row][col - 1] = "."
                     moved = True
-                else:
-                    d_grid[row][col] = r_grid[row][col]
+                    col += 1
+                col += 1
+                
 
-        d_grid[2][-1] = "."
-
-        for row in range(len(r_grid)):
-            for col in range(len(r_grid[0])):
-                if d_grid[row][col] == "." and d_grid[row - 1][col] == "v":
-                    r_grid[row][col] = "v"
-                    r_grid[row - 1][col] = "."
+        for col in range(len(grid[0])):
+            last_was_occupied = False
+            row = 0
+            while row < len(grid):
+                if grid[row][col] == "." and grid[row - 1][col] == "v" \
+                    and (row != len(grid) - 1 or not last_was_occupied):
+                    if row == 0:
+                        last_was_occupied = True
+                    grid[row][col] = "v"
+                    grid[row - 1][col] = "."
                     moved = True
-                else:
-                    r_grid[row][col] = d_grid[row][col]
-
+                    row += 1
+                row += 1
         steps += 1
 
     return steps
 
 
-aoc_util.run_with_data_str(task, data_str_easy)
+#aoc_util.run_with_data_str(task, data_steasy)
 aoc_util.run_with_data_str(task, data_str)
 aoc_util.run_with_data_set(task, day)
